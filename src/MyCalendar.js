d903b38f7884144d5c97207e4f1ca7ccf9889a00
justin8491 <justin77748292@gmail.com>
2023-02-04 19:09:20 +0900

    한글 번경중 commit

--- src/MyCalendar.js
+++ src/MyCalendar.js
@@ -11,32 +11,22 @@ class MyCalendar extends Component {
           defaultView="dayGridMonth"
           plugins={[dayGridPlugin]}
           events={[
+            {},
             { title: "오늘은 캘린더 작성 날", date: "2023-02-01" },
             { title: "리액트 진도 빼기", date: "2023-02-02" },
-          ]}
-          monthName={[
-            "1월",
-            "2월",
-            "3월",
-            "4월",
-            "5월",
-            "6월",
-            "7월",
-            "8월",
-            "9월",
-            "10월",
-            "11월",
-            "12월",
           ]}
         />
       </div>
     );
   }
+
+  onClickEvents = (title, date) => {
+    let App = document.querySelector("#App");
+    let target = App.querySelector(".fc-daygrid-day");
+    target.onclick = (e) => {
+      alert("event Click");
+    };
+  };
 }
-let App = document.querySelector("#App");
-let event = App.querySelector("a");
-event.onclick = (e) => {
-  alert(e);
-};
 
 export default MyCalendar;
