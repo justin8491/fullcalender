878fde0611cc8b9271eee4aba7171ee953332f9c
justin8491 <justin77748292@gmail.com>
2023-02-05 14:54:47 +0900

    주석 및 Calendar 한글화

--- src/MyCalendar.js
+++ src/MyCalendar.js
@@ -6,7 +6,12 @@ import * as bootstrap from "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./MyCalendar.css";
 
+//구동 전 설치 yarn 설정
+//yarn add @fullcalendar/timegrid
+//yarn add @fullcalendar/interaction
+//yarn add bootstrap
 function MyCalendar() {
+  //이벤트 하드 코딩
   const event = [
     {
       title: "이벤트 버튼",
@@ -19,16 +24,18 @@ function MyCalendar() {
     <div className="App">
       <FullCalendar
         initialView="dayGridMonth"
-        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]} //플러그인 설치 및 적용
         headerToolbar={{
-          start: "prev today", // will normally be on the left. if RTL, will be on the right
-          center: "title",
-          end: "dayGridMonth,timeGridWeek,timeGridDay next", // will normally be on the right. if RTL, will be on the left
+          start: "prev today", // 이전달, 오늘
+          center: "title", // 달력 개월 수
+          end: "dayGridMonth,timeGridWeek,timeGridDay next", // 이번주 할일, 날짜 별 할일, 다음달
         }}
         height={"30rem"}
         events={event}
+        //hover 시 해당 요소 Popover
         eventDidMount={(info) => {
           return new bootstrap.Popover(info.el, {
+            // 요소 el = 엘리먼트 대상을 설명
             title: info.event.title,
             placement: "auto",
             trigger: "hover",
@@ -38,10 +45,11 @@ function MyCalendar() {
             html: true,
           });
         }}
+        // 클릭 시 함수
         eventClick={(e) => {
           alert(e.event.start);
-          console.log(e.event.start);
         }}
+        locales={"ko"} // 한글
       />
     </div>
   );
